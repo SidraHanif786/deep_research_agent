e7a52f9d070f799b71e5e25ca774360babe89f47
Sidra Hanif <sidrahanif913@gmail.com>
2025-09-07 16:12:49 +0500

    tool calling added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel, set_tracing_disabled, function_tool
 import os
 from dotenv import load_dotenv
 import asyncio
@@ -11,25 +11,41 @@ MODEL= os.getenv("MODEL")
 
 set_tracing_disabled(disabled=True)
 
-# Running agent
+# Gemini client
 external_client: AsyncOpenAI = AsyncOpenAI(
     api_key=GEMINI_API_KEY,
     base_url=BASE_URL,
 )
-
+# Model
 llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
     model=MODEL,
     openai_client=external_client
 )
 
+# Define tools 
+@function_tool
+def multiply(a: int, b: int) -> int:
+    """Exact multiplication ___ """
+    return a * b
+
+@function_tool
+def sum(a: int, b: int) -> int:
+    """Exact addition ___ """
+    return a + b
+
+# Agent
 math_agent: Agent = Agent(
     name="MathAgent",
-    instructions="You are a helpful math assistant.",
-    model=llm_model) # gemini-2.5 as agent brain - chat completions
+    instructions="You are a helpful math assistant."
+                "Always use tools for math questions. Always follow DMAS rule (division, multiplication, addition, subtraction). "
+                "Explain answers clearly and briefly for beginners.",
+    model=llm_model,
+    tools=[multiply, sum]
+    )
 
 async def main():
 
-    result: Runner = await Runner.run(math_agent, "Tell me about recursion in programming.")
+    result: Runner = await Runner.run(math_agent, "what is 19 + 23 * 2?")
     print(result.final_output)
 
 asyncio.run(main())
